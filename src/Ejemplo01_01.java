c740cd8f0c0dba88f357824ab227d02ef12c5928
Rafa <97rafa97@gmail.com>
2024-01-06 18:15:49 +0100

    Iniciación

---
+++ src/Ejemplo01_01.java
@@ -0,0 +1,10 @@
+public class Ejemplo01_01 {
+    /*
+    * Public-->puedo llamar al método main desde cualquier otra clase
+    * static-->no se necesita tener un objeto para usar el método
+    * void-->el método no deolverá ningún resultado
+    * main*/
+    public static void main (String [] args){
+
+    }
+}
