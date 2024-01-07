c9fe82301efaf29b30f3ef99675f0b4025c9f497
Rafa <97rafa97@gmail.com>
2024-01-07 18:24:15 +0100

    Operadores aritméticos.2

--- src/Ejemplo02_04.java
+++ src/Ejemplo02_04.java
@@ -1,3 +1,10 @@
 public class Ejemplo02_04 {
-
+    public static void main (String [] args){
+        int divEntera=7/2;
+        int modulo=7%2; //El módulo se interpreta como el residuo//
+        double divReal=7.0/2.0;
+        System.out.println("Valor división entera es "+divEntera);
+        System.out.println("Valor del módulo es "+modulo);
+        System.out.println("El valor de la div real es "+divReal);
+    }
 }
